e443f874573576db55a363e73a5b4ae716d998b7
Albert Nevado <albert.nevado@gmail.com>
2021-10-16 00:26:58 +0200

    Review configs to parse and run test files
    Also, export babel configuration to its dedicated file to make it easier to configure.

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,15 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.(js|jsx)?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: ['/node_modules/', '/public/'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  moduleNameMapper: {
+    '\\.(css|scss|less)$': 'identity-obj-proxy',
+  },
 };
